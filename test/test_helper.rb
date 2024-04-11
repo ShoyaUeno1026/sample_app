554ad8e1b24f04a53414e6ea0ce70c5d57bcd093
Shoya Ueno <166480301+ShoyaUeno1026@users.noreply.github.com>
2024-04-11 04:19:19 +0000

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,11 +5,13 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+  # 指定のワーカー数でテストを並列実行する
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
+  # test/fixtures/*.ymlのfixtureをすべてセットアップする
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
